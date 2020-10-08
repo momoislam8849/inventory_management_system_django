2f05d52861bd494c97021706a159600b1581cfc6
momoislam8849 <shampa15-8849@diu.edu.bd>
2020-10-08 19:15:09 +0600

    Adding new files

--- stockmgmt/forms.py
+++ stockmgmt/forms.py
@@ -5,3 +5,15 @@ class StockCreateForm(forms.ModelForm):
    class Meta:
      model = Stock
      fields = ['category', 'item_name', 'quantity']
+     widgets = {
+			'category': forms.TextInput(attrs={'class':'form-control','placeholder':'Category name'}),
+			'item_name': forms.TextInput(attrs={'class':'form-control','placeholder':'Item name'}),
+			'quantity': forms.TextInput(attrs={'class':'form-control'}),
+			}
+
+class StockSearchForm(forms.ModelForm):
+   class Meta:
+     model = Stock
+     fields = ['item_name']
+
+		
